b46b9f53fbba638b548cd63cf43e42c8660b395d
titov <dtitov190@gmail.com>
2023-06-27 12:42:13 +0300

    HM 8

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
-import './App.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './components/App';
 import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux';
+import store from './state/store';
+import './App.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(
+    <Provider store={store}>
+        <App />
+    </Provider>
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
--- src/pages/Popular/index.js
+++ src/pages/Popular/index.js
@@ -1,83 +1,57 @@
-import React, { useState, useEffect } from "react";
-import { fetchPopularRepos } from '../../api/requests';
-import Tabs from '../../components/navigation/Tabs';
+import Tabs from '../../components/navigation/Tabs'
+import { useSelector } from 'react-redux';
 import Loader from '../../components/Loader';
 
-import { useSearchParams } from "react-router-dom";
 
-const Popular = () => {
-    const [searchParams, setSearchParams] = useSearchParams();
-    const selectedLanguage = searchParams.get("language") || "All";
-    const [loading, setLoading] = useState(false);
-    const [repos, setRepos] = useState([]);
-    const [error, setError] = useState(null);
-
-    const languages = ["All", "JavaScript", "Ruby", "Java", "CSS", "Python"];
-
-    const handleTabClick = (language) => {
-        setSearchParams({ language });
-    };
-
-    useEffect(() => {
-        const fetchData = async () => {
-            setLoading(true);
-            try {
-                const data = await fetchPopularRepos(selectedLanguage);
-                setRepos(data);
-            } catch (err) {
-                setError(err);
-            } finally {
-                setLoading(false);
-            }
-        };
-        fetchData();
-    }, [selectedLanguage]);
-
-
-
-    const RepList = ({ repos }) => {
-        return (
-            <ul className="popular-list">
-                {repos.map((item, index) => {
-                    return (
-                        <li key={item.id} className="popular-item">
-                            <div className="popular-rank">#{index + 1}</div>
-                            <ul className="space-list-items">
-                                <li>
-                                    <img
-                                        className="avatar"
-                                        src={item.owner.avatar_url}
-                                        alt="Avatar"
-                                    />
-                                </li>
-                                <li>
-                                    <a href={item.html_url}>{item.name}</a>
-                                </li>
-                                <li>@{item.owner.login}</li>
-                                <li>{item.stargazers_count}</li>
-                            </ul>
-                        </li>
-                    );
-                })}
-            </ul>
-        );
-    };
-
-    const renderList = () => {
-        return loading ? <Loader /> : <RepList repos={repos} />;
-    };
+const RepList  = () => {
+    const repos = useSelector((state) => state.popularReducer.repos);
 
     return (
-        <>
-            <Tabs
-                languages={languages}
-                selectedLanguage={selectedLanguage}
-                onTabClick={handleTabClick}
-                loading={loading}
-            />
-            {renderList()}
-        </>
+        <ul className='popular-list'>
+            {repos.map((repo, index) => {
+                return (
+                    <li key={repo.id} className='popular-item'>
+                        <div className='popular-rank'>#{index + 1}</div>
+                        <ul className='space-list-items'>
+                            <li>
+                                <img
+                                    className='avatar'
+                                    src={repo.owner.avatar_url}
+                                    alt='Avatar'
+                                />
+                            </li>
+                            <li>
+                                <a href={repo.html_url} target='_blank'>
+                                    {repo.name}
+                                </a>
+                            </li>
+                            <li>@{repo.owner.login}</li>
+                            <li>{repo.stargazers_count}</li>
+                        </ul>
+                    </li>
+                );
+            })}
+        </ul>
     );
 };
+
+const Repository = () => {
+    const loading = useSelector((state) => state.popularReducer.loading);
+    const error = useSelector((state) => state.popularReducer.error);
+
+    if (error) return <p>Error: {error}</p>;
+
+    const renderRepos = () => {
+        return loading ? <Loader /> : <RepList />;
+    };
+
+    return <>{renderRepos()}</>;
+};
+const Popular = () => (
+    <>
+        <Tabs />
+        <Repository />
+    </>
+);
 
 export default Popular;
